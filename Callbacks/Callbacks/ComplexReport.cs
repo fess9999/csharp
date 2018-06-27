eb7e5fac4950fc1967e05f0ad95c459ca4d22365
SAPFIR\Fedorov_AV <fedorov_av@nposapfir.ru>
2018-06-27 12:21:21 +0500

    процессы, потоки и таски

--- Callbacks/Callbacks/ComplexReport.cs
+++ Callbacks/Callbacks/ComplexReport.cs
@@ -8,28 +8,48 @@ namespace Callbacks
 
     public class ComplexReport
     {
+        private readonly string result;
+
+        public ComplexReport(string result) => this.result = result;
+
         public string BuildSynchronously()
         {
             Thread.Sleep(5000);
-            return "42";
+            return result;
         }
 
-        public void BuildAsynchronously(ReportResultDelegate callback)
+        public async Task<string> BuildReportAsync()
         {
-            Task.Run(() => Thread.Sleep(5000)).ContinueWith(task => callback("42"));
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Preparing to build report...");
+
+            await Task.Delay(2000); //долгая операция, имитирующая обращение к базе данных
+
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Done. ");
+
+            return result;
         }
 
-        public void BuildWithAction(Action<string> action)
+        public void BuildWithCancelation(CancellationToken cancellationToken)
         {
-            Task.Run(() => Thread.Sleep(5000)).ContinueWith(task => action("42"));
+            Task.Run(() =>
+                {
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Composing report....");
+                        Thread.Sleep(1000);
+                    }
+                }, cancellationToken)
+                .ContinueWith((task, o) => Console.WriteLine("Report building has been canceled"),
+                    TaskContinuationOptions.OnlyOnCanceled);
         }
 
-        public void BuildWithFunc(Func<string, bool> func)
-        {
-            Task.Run(() => Thread.Sleep(5000)).ContinueWith(task =>
-            {
-                Console.WriteLine(func("Report has been being built too long. Abort?") ? "Aborted" : "Continued");
-            });
-        }
+        public Task BuildAsynchronously(ReportResultDelegate callback) =>
+            Task.Run(() => Thread.Sleep(1000)).ContinueWith(task => callback(result));
+
+        public void BuildWithAction(Action<string> action) =>
+            Task.Run(() => Thread.Sleep(5000)).ContinueWith(task => action(result));
+
+        public void BuildWithFunc(Func<string, bool> func) => Task.Run(() => Thread.Sleep(5000)).ContinueWith(task =>
+            Console.WriteLine(func("Report has been being built too long. Abort?") ? "Aborted" : "Continued"));
     }
 }
--- Callbacks/Callbacks/Program.cs
+++ Callbacks/Callbacks/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MoreLinq;
 
 namespace Callbacks
 {
     class Program
     {
+        private static int a = 5;
+
+        private static readonly Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+        private static readonly CancellationTokenSource tokenSource = new CancellationTokenSource();
+
         static void Main(string[] args)
         {
-            var report = new ComplexReport();
+            var report = new ComplexReport("42");
             var anotherReport = new AnotherComplexReport();
             var eventBasedReport = new EventBasedReport();
 
-            Console.WriteLine("Let's build a report. Press any key to begin");
-            Console.ReadKey();
+            //Console.WriteLine("Let's build a report. Press any key to begin");
+            //Console.ReadKey();
 
-            SyncReport(report);
+            //SyncReport(report);
             //AsyncReport(report);
             //MulticastReport(report);
             //ActionReport(report);
@@ -23,8 +35,98 @@ namespace Callbacks
             //AnonymousEventReport(eventBasedReport);
             //LambdaEventReport(eventBasedReport);
 
+            ProcessFun();
+            //ThreadsFun();
+            //RaceCondition1();
+            //RaceCondition2();
+
+            //CancelableReport(report);
+            //SeveralReportsSimultaneously();
+            //AsyncAwaitReport(report).Wait();
+
             while (true)
+            {
                 Console.WriteLine($"Echo: {Console.ReadLine()}");
+                tokenSource.Cancel();
+            }
+        }
+
+        private static async Task AsyncAwaitReport(ComplexReport report)
+        {
+            PrintReportResult(await report.BuildReportAsync());
+        }
+
+        private static void SeveralReportsSimultaneously()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var tasks = Enumerable.Range(0, 10)
+                .Select(i => new ComplexReport(i.ToString()).BuildAsynchronously(PrintReportResult)).ToArray();
+
+            Task.WaitAll(tasks);
+            Console.WriteLine($"All reports have been built in {stopwatch.Elapsed}");
+        }
+
+        private static void CancelableReport(ComplexReport report) => report.BuildWithCancelation(tokenSource.Token);
+
+        private static void RaceCondition1()
+        {
+            while (true)
+            {
+                Console.ReadKey();
+                a = 5;
+                var thread1 = new Thread(() => a = a + 1);
+                var thread2 = new Thread(() => a = a * 2);
+
+                thread1.Start();
+                thread2.Start();
+
+                thread1.Join();
+                thread2.Join();
+
+                Console.WriteLine(a);
+            }
+        }
+
+        private static void RaceCondition2()
+        {
+            var thread1 = new Thread(AddValues);
+            var thread2 = new Thread(AddValues);
+
+            thread1.Start();
+            thread2.Start();
+        }
+
+        private static void AddValues() => Enumerable.Range(0, 10)
+            .ForEach(i => AddKeyValue(i, $"value {Thread.CurrentThread.ManagedThreadId}"));
+
+        private static void AddKeyValue(int key, string value)
+        {
+            lock (dictionary)
+            {
+                if (!dictionary.ContainsKey(key))
+                    dictionary.Add(key, value);
+            }
+        }
+
+        private static void AddKeyValueSynchronized(int key, string value)
+        {
+            if (!dictionary.ContainsKey(key))
+                dictionary.Add(key, value);
+        }
+
+        private static void ThreadsFun()
+        {
+            var currentThread = Thread.CurrentThread;
+            Console.WriteLine(currentThread.Name);
+            Console.WriteLine(currentThread.ManagedThreadId);
+            Console.WriteLine(currentThread.Priority);
+            Console.WriteLine(currentThread.CurrentCulture);
+        }
+
+        private static void ProcessFun()
+        {
+            var processes = Process.GetProcesses();
+            processes.ForEach(process => Console.WriteLine($"{process.Id} {process.ProcessName}"));
         }
 
         private static void EventReport(EventBasedReport eventBasedReport)
@@ -97,7 +199,7 @@ namespace Callbacks
 
         private static void PrintReportResult(string reportResult)
         {
-            Console.WriteLine($"Report result: {reportResult}");
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Report result: {reportResult}");
         }
 
         private static void SendReportResult(string reportResult)
